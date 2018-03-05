29d4d9bfabcea5493a7d563c5b1d36446bce8662
niefapeng <36945190+niefapeng@users.noreply.github.com>
2018-03-05 09:25:44 +0800

    Add files via upload

--- homework1/Include/list.h
+++ homework1/Include/list.h
@@ -1,1 +1,16 @@
 
+typedef struct Node
+{
+	int data;
+	struct Node*next;
+}Node,*List;
+
+int Initlist(List plist);
+int Insert_head(List plist,int val);
+int Insert_tail(List plist,int val);
+int Insert_pos(List plist,int val,int pos);
+int Delete_head(List plist);
+int Delete_tail(List plist);
+int Delete_pos(List plist,int pos);
+Node* Find_val(List plist,int val);
+Node* Reverse(List plist);
