6d74bc268eb6d085d53bb36f90f415a335d3ccbb
niefapeng <36945190+niefapeng@users.noreply.github.com>
2018-03-05 09:33:27 +0800

    Add files via upload

--- homework1/Reverse/reverse.c
+++ homework1/Reverse/reverse.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+#include"../Include/list.h"
+//insert head
+Node* Reverse(List plist)
+{
+	if(plist==NULL || plist->next==NULL || plist->next->next==NULL)
+	{
+		return NULL;
+	}
+	Node*p;
+	p = plist->next;
+	plist->next = NULL;//only head node,no data
 
+	Node*q;
+	while(p!=NULL)
+	{
+		q = p->next;
+
+		p->next = plist->next;
+		plist->next = p;
+		p = q;
+	}
+	return plist;
+}
